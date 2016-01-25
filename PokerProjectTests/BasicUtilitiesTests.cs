1ee2794a73eada1e913f215b3b9d8083ff6b5af5
ROSSFilipov <ruslanfilipov@yahoo.com>
2016-01-25 16:38:45 +0200

    Refactoring

---
+++ PokerProjectTests/BasicUtilitiesTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Poker.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Poker;
+
+    [TestClass]
+    public class BasicUtilitiesTests
+    {
+        [TestMethod]
+        public void Test_RandomGeneratorZeroRange()
+        {
+            int number = RandomGenerator.Next(0);
+            Assert.AreEqual(0, 0, "The Random Generator utility is not working properly.");
+        }
+
+        [TestMethod]
+        public void Test_RandomGeneratorSpecificRange()
+        {
+            int number = RandomGenerator.Next(0, 5);
+            bool isInRange = number >= 0 && number <= 5;
+            Assert.AreEqual(true, isInRange, "The Random Generator utility is not working properly.");
+        }
+
+        [TestMethod]
+        public void Test_RandomGeneratorNegativeRange()
+        {
+            int number = RandomGenerator.Next(-10, -5);
+            bool isInRange = number >= -10 && number <= -5;
+            Assert.AreEqual(true, isInRange, "The Random Generator utility is not working properly.");
+        }
+    }
+}
---
+++ PokerProjectTests/PokerDatabaseTests.cs
@@ -0,0 +1,14 @@
+﻿namespace PokerProjectTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Poker.Interfaces;
+    using Poker.Models;
+    using Poker.Core;
+
+    [TestClass]
+    public class PokerDatabaseTests
+    {
+        IPokerDatabase database = new PokerDatabase();
+    }
+}
