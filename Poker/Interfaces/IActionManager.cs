e35241c9b44b693eae21f285eff63602d3350a7e
ROSSFilipov <ruslanfilipov@yahoo.com>
2016-01-27 17:00:16 +0200

    Refactoring

--- Poker/Interfaces/IActionManager.cs
+++ Poker/Interfaces/IActionManager.cs
@@ -3,14 +3,38 @@
     using System;
     using System.Windows.Forms;
 
+    /// <summary>
+    /// The interface is responsible to provide all possible poker game moves
+    /// a player can do.
+    /// Poker methods description are written in accordance with:
+    /// http://learn.pokernews.com/poker-strategy-theory/bet-check-raise-fold-what-it-means-1036.htm
+    /// </summary>
     public interface IActionManager
     {
+        /// <summary>
+        /// The poker table which is affected by the methods used in the current Action Manager class.
+        /// </summary>
         IGameTable GameTable { get; set; }
 
+        /// <summary>
+        /// When checking, a player declines to make a bet.
+        /// </summary>
+        /// <param name="currentPlayer">The player who Checks.</param>
         void Check(IPlayer currentPlayer, ref bool isRaisingActivated);
 
+        /// <summary>
+        /// Fold is to discard one's hand and forfeit interest in the current pot. 
+        /// No further bets are required by the folding player, but the player but the Folded player cannot win.
+        /// </summary>
+        /// <param name="currentPlayer">The player who Folded.</param>
         void Fold(IPlayer currentPlayer, ref bool isRaisingActivated);
 
+        /// <summary>
+        /// Calling is the mechanism used to call a bet. This is essentially matching the amount
+        /// that has been put in by another player in the form of a bet or a raise. 
+        /// If nobody calls, the hand is over and the uncalled player wins the hand.
+        /// </summary>
+        /// <param name="currentPlayer">The player who Calls.</param>
         void Call(IPlayer currentPlayer, ref bool isRaisingActivated, int globalCall, ref TextBox potTexBox);
 
         void Raised(IPlayer currentPlayer, ref bool isRaisingActivated, ref int globalRaise, ref int globalCall, ref TextBox potTextBox);
@@ -19,6 +43,9 @@
 
         void PH(IPlayer currentPlayer, int globalCall, TextBox potTextBox, ref int globalRaise, ref bool isRaisingActivated, int globalRounds, int n, int n1, int r);
 
+        /// <summary>
+        /// The method is used to provide utility to the non-human controlled players.
+        /// </summary>
         void AI(IPlayer currentPlayer, int globalCall, TextBox potTextBox, ref int globalRaise, ref bool isRaisingActivated, ref int globalRounds, int name);
     }
 }
--- Poker/Interfaces/IBotEraser.cs
+++ Poker/Interfaces/IBotEraser.cs
@@ -2,24 +2,48 @@
 {
     using System;
 
+    /// <summary>
+    /// The interface/class is responsible for resetting the data of all non-human controlled players.
+    /// It directly integrates with the bot database and iterates through its collection of bots.
+    /// </summary>
     public interface IBotEraser
     {
+        /// <summary>
+        /// Sets the Type property of each bot to the default value of Type. Default values
+        /// could be found in the PokerGameConstants class.
+        /// </summary>
+        /// <param name="pokerDatabase"></param>
         void EraseBotType(IPokerDatabase pokerDatabase);
 
         void EraseBotPower(IPokerDatabase pokerDatabase);
 
+        /// <summary>
+        /// Turns each bot panel's visibility to false.
+        /// </summary>
         void DisableBotPanel(IPokerDatabase pokerDatabase);
 
         void EraseBotRaise(IPokerDatabase pokerDatabase);
 
         void EraseBotCall(IPokerDatabase pokerDatabase);
 
+        /// <summary>
+        /// Sets the text of each box indicating the bot's status to "".
+        /// </summary>
         void EraseBotStatusText(IPokerDatabase pokerDatabase);
 
+        /// <summary>
+        /// Sets the Folded boolean property of each bot to false.
+        /// </summary>
         void UnFoldBots(IPokerDatabase pokerDatabase);
 
+        /// <summary>
+        /// Sets the OutOfChips property of each bot to false.
+        /// </summary>
         void EnableBotChips(IPokerDatabase pokerDatabase);
 
+        /// <summary>
+        /// Sets the CanPlay boolean property of all bots to false.
+        /// </summary>
         void DisableBots(IPokerDatabase pokerDatabase);
     }
 }
